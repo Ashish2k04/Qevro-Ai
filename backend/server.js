68329ede3c412035751043b81850ec74317de9b7
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-05 12:44:42 +0530

    Enhanced the server.js code with Promise chaining

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,16 @@ import connectDB from './src/config/database.js';
 
 const PORT = process.env.PORT || 8000
 
-connectDB();
-
-app.listen(PORT, ()=>{
+connectDB()
+.then(()=>{
+    app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
 })
+})
+.catch((err)=>{
+    console.log(`MongoDB is failed to connected`, err.message)
+    process.exit(1);
+});
+
+
 

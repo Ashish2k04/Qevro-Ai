cacb2fb2ee044ef98e4f903608332370c7fa5981
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-06 23:43:25 +0530

    Fixing the bug inside Form.jsx

--- frontend/src/features/components/Form.jsx
+++ frontend/src/features/components/Form.jsx
@@ -3,7 +3,15 @@
 const Form = () => {
   return (
     <div>
-        
+         <input
+                id="email"
+                type="email"
+                placeholder="you@example.com"
+                className="w-full h-12 px-4 rounded-lg border border-gray-300
+                bg-white text-sm text-gray-900 outline-none transition-all
+                placeholder:text-gray-400
+                focus:border-indigo-500 focus:ring-2 focus:ring-indigo-100"
+              />
     </div>
   )
 }

425bdcf89f4f09efd8803af846875bf92579cd3c
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-06 23:57:33 +0530

    Adding showPassword useState in register.jsx

--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -7,7 +7,6 @@ const Register = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
   const [showPassword, setShowPassword] = useState(false)
 
   return (

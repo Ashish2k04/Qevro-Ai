aec13907d9c38cca235e83df794b48233b85fca2
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-07 15:00:58 +0530

    written the logic inside the auth.slice.js

--- frontend/src/features/auth/auth.slice.js
+++ frontend/src/features/auth/auth.slice.js
@@ -1,8 +1,22 @@
 import {createSlice} from '@reduxjs/toolkit';
+import { act } from 'react';
 
 const authSlice = createSlice({
     name: "auth",
     initialState: {
-        
+        user: null,
+        loading: false,
+        error: null
+    },
+    reducers: {
+        setUser: (state, action) => {
+            state.user = action.payload
+        },
+        setLoading: (state, action) => {
+            state.loading = action.payload
+        },
+        setError: (state, action) => {
+            state.error = action.payload
+        }
     }
 })

8b9917595116eb8d49cdc7f373880f869ee60816
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-07 19:25:25 +0530

    Installed morgan in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ import morgan from 'morgan';
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(morgan("dev"));
+app.use(morgan("dev"))
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,

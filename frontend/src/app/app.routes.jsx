a5569aae78cd8a3408ae7bfaa8f75ca87bb37e66
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-10 13:06:53 +0530

    Adding routes of Dashboard.jsx inside app.routes.jsx

--- frontend/src/app/app.routes.jsx
+++ frontend/src/app/app.routes.jsx
@@ -1,6 +1,7 @@
 import {createBrowserRouter} from 'react-router';
 import Login from '../features/auth/pages/Login.jsx';
 import Register from '../features/auth/pages/Register.jsx';
+import Dashboard from '../features/chat/Dashboard.jsx';
 
 export const appRoutes = createBrowserRouter([
     {
@@ -14,5 +15,9 @@ export const appRoutes = createBrowserRouter([
     {
         element: <h1>Home</h1>,
         path: '/'
+    },
+    {
+        element: <Dashboard />,
+        path: '/dashboard'
     }
 ])

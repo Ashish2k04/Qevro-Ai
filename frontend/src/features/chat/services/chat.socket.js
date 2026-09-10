f8597aedb5e399abc45b1e02eb7bb2f96cab29f2
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-10 22:22:41 +0530

    Fixed the path of dashboard inside route and backend url inside chat.socket.js

--- frontend/src/features/chat/services/chat.socket.js
+++ frontend/src/features/chat/services/chat.socket.js
@@ -1,11 +1,11 @@
 import {io} from 'socket.io-client';
 
 export const initializeSocketConnection = () =>{
-    const socket = io("http://localhost:3000/api", {
+    const socket = io("http://localhost:3000", {
         withCredentials: true
     })
 
     socket.on('connect', ()=>{
-        console.log('SocketIO is connected.')
+        console.log('Connected to socketIo server.')
     })
 }

8f96b0d043e70f9c23fb4ffe9a1bb779ff5a73a7
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-10 14:08:17 +0530

    Removing useAuthinit.js insted using useEffect inside app.jsx

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,7 +1,13 @@
 import { appRoutes } from './app.routes.jsx'
 import { RouterProvider } from 'react-router'
+import { useAuth } from '../features/auth/hook/useAuth.js'
+import { useEffect } from 'react'
 
 const App = () => {
+  const auth = useAuth();
+  useEffect(()=>{
+     auth.handleGetMe();
+  }, [])
   return (
     <div>
         <RouterProvider router={appRoutes}/>

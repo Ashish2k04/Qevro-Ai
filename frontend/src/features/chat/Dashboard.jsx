aed837261806b9ea9fb92727262d169c6062279c
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-10 13:04:36 +0530

    Using useSelector from react-redux inside Dashboard

--- frontend/src/features/chat/Dashboard.jsx
+++ frontend/src/features/chat/Dashboard.jsx
@@ -1,7 +1,11 @@
+import {useSelector} from 'react-redux'
 
 const Dashboard = () => {
+    const {user} = useSelector(state => state.auth.user);
   return (
-    <div>Dashboard</div>
+    <div>
+     {user}
+    </div>
   )
 }
 

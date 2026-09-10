d9ebff45ace1339ff0b4b44684bb6ff6cdc93c5a
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-10 22:16:04 +0530

    Using useEffect hook in Dashboard to get socket connection

--- frontend/src/features/chat/pages/Dashboard.jsx
+++ frontend/src/features/chat/pages/Dashboard.jsx
@@ -1,8 +1,15 @@
 import {useSelector} from 'react-redux'
+import { useEffect } from 'react';
+import { useChat } from '../hooks/useChat';
 
 const Dashboard = () => {
     const {user} = useSelector(state => state.auth);
+    const {initializeSocketConnection} = useChat();
     console.log(user)
+
+    useEffect(()=>{
+      initializeSocketConnection();
+    }, [])
   return (
     <div>
      Dashboard

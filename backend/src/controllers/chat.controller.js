6753e1da797cdc54f3800c2439a2cba0fcb315ae
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-12 14:14:32 +0530

    Fixed askAi variable

--- backend/src/controllers/chat.controller.js
+++ backend/src/controllers/chat.controller.js
@@ -3,12 +3,12 @@ import { askAi } from '../services/ai.service.js';
 export async function sendMessagesController(req,res,next) {
     try{
     const {message} = req.body;
-    const ai_answer = await askAi(message);
+    const ai_reply = await askAi(message);
 
     return res.status(201).json({
         message: "Reply of your message is created successfully.",
         success: true,
-        answer: ai_answer
+        answer: ai_reply
     });
    }
    catch(err){

4c8a14678c3340078380bfbe72a1808ed02feffe
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-12 13:35:49 +0530

    Shifted controller succesfully from auth controller into chat controller

---
+++ backend/src/controllers/chat.controller.js
@@ -0,0 +1,17 @@
+
+export async function sendMessages(req,res,next) {
+    try{
+    const {question} = req.body;
+    const ai_answer = await askAi(question);
+
+    return res.status(201).json({
+        message: "Your answer created successfully.",
+        success: true,
+        answer: ai_answer
+    });
+   }
+   catch(err){
+      err.status = 500;
+      next(err)
+   }
+};

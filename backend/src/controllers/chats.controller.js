5e783c6cf061e6cc511b295382a943ec73241e21
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-13 18:33:07 +0530

    Message model connected to chat controller and added variables in bruno

--- backend/src/controllers/chats.controller.js
+++ backend/src/controllers/chats.controller.js
@@ -1,5 +1,6 @@
 import { askAi, generateChatTitle } from '../services/ai.service.js';
 import chatModel from '../models/chat.model.js';
+import messageModel from '../models/message.model.js';
 
 export async function sendMessagesController(req,res,next) {
     try{
@@ -11,13 +12,24 @@ export async function sendMessagesController(req,res,next) {
         generateChatTitle(message)
     ]);
 
-    await chatModel.create({user: id, title: ai_Title});
+    const chat = await chatModel.create({
+        user: id, 
+        title: ai_Title
+    });
+
+    const response = await messageModel.create({
+        chat: chat._id,
+        content: ai_reply,
+        role: "ai"
+    })
 
     return res.status(201).json({
         message: "Reply of your message is created successfully.",
         success: true,
         title: ai_Title,
-        answer: ai_reply
+        answer: ai_reply,
+        chat,
+        response
     });
    }
    catch(err){

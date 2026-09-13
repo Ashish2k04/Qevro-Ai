dacfaa11c2a100244ef52d84a8650e4377cc55d1
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-13 18:13:25 +0530

    Connected chat model with chats controller

--- backend/src/controllers/chats.controller.js
+++ backend/src/controllers/chats.controller.js
@@ -1,13 +1,17 @@
 import { askAi, generateChatTitle } from '../services/ai.service.js';
+import chatModel from '../models/chat.model.js';
 
 export async function sendMessagesController(req,res,next) {
     try{
     const {message} = req.body;
+    const {id} = req.user;
 
     const [ai_reply, ai_Title] = await Promise.all([
         askAi(message),
         generateChatTitle(message)
-    ])
+    ]);
+
+    await chatModel.create({user: id, title: ai_Title});
 
     return res.status(201).json({
         message: "Reply of your message is created successfully.",

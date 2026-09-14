6043e8c59d9b502a868c1af7ed81c5105850949d
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-14 19:28:29 +0530

    New route added to chats route

--- backend/src/routes/chats.route.js
+++ backend/src/routes/chats.route.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
 import { tokenVerification } from '../middlewares/auth.middleware.js';
-import {sendMessagesController, getChats} from '../controllers/chats.controller.js'
+import {sendMessagesController, getChats, getMessages} from '../controllers/chats.controller.js'
 
 const chatRouter = Router();
 
 chatRouter.post('/message', tokenVerification, sendMessagesController);
 chatRouter.get('/get-chats', tokenVerification, getChats);
+chatRouter.get('/get-messages', tokenVerification, getMessages);
 
 export default chatRouter;

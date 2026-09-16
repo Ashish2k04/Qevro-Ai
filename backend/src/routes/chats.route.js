27b388e2bf54dc0f91cbe0a2e9724ddd24f5ca3d
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-16 14:16:09 +0530

    importing the deleteChat controller inside chats.route.js

--- backend/src/routes/chats.route.js
+++ backend/src/routes/chats.route.js
@@ -1,11 +1,12 @@
 import {Router} from 'express';
 import { tokenVerification } from '../middlewares/auth.middleware.js';
-import {sendMessagesController, getChats, getMessages} from '../controllers/chats.controller.js'
+import {sendMessages, getChats, getMessages, deleteChat} from '../controllers/chats.controller.js'
 
 const chatRouter = Router();
 
-chatRouter.post('/message', tokenVerification, sendMessagesController);
+chatRouter.post('/message', tokenVerification, sendMessages);
 chatRouter.get('/get-chats', tokenVerification, getChats);
 chatRouter.get('/get-messages/:chatId', tokenVerification, getMessages);
+chatRouter.delete('/delete-chat/:chatId', tokenVerification, deleteChat);
 
 export default chatRouter;

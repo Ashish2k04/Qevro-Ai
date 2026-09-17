b248686621c964fd7160930628eccd9fa4018966
Ashish2k04 <iamashishtiwari203@gmail.com>
2026-09-17 11:38:27 +0530

    All the api variables and state variables are imported in useChat Hook along with useDispatch from react-redux

--- frontend/src/features/chat/hooks/useChat.js
+++ frontend/src/features/chat/hooks/useChat.js
@@ -1,5 +1,12 @@
 import {initializeSocketConnection} from '../services/chat.socket.js';
+import {useDispatch} from 'react-redux';
+import {sendMessages, getMessages, getChats, deletChat} from '../services/chat.api.js';
+import {setChats, setcurrentChatId, setLoading, setError} from '../chat.slice.js';
 
 export const useChat = () => {
+
+    const dispatch = useDispatch();
+
+
     return{initializeSocketConnection}
 }
